94ffcce894443b44ef60cccf38a58c63a9ac3b2f
Aime-Patrick <aimepatrick64@gmail.com>
2026-08-20 06:13:11 +0200

    fix: add timeZone to NextIntlClientProvider to fix ENVIRONMENT_FALLBACK error

--- src/components/providers/i18n-provider.tsx
+++ src/components/providers/i18n-provider.tsx
@@ -10,7 +10,7 @@ interface I18nProviderProps {
 
 export function I18nProvider({ locale, messages, children }: I18nProviderProps) {
   return (
-    <NextIntlClientProvider locale={locale} messages={messages}>
+    <NextIntlClientProvider locale={locale} messages={messages} timeZone="Africa/Kigali">
       {children}
     </NextIntlClientProvider>
   );
